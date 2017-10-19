7ade44f1501d7e034ff216733dffc29d7d4ae2f8
liuyuanbo <guxiagufei@163.com>
2017-10-19 18:09:02 +0800

    11

--- custom2/src/main/java/com/liu/springcloud/custom2/feign/FeignService.java
+++ custom2/src/main/java/com/liu/springcloud/custom2/feign/FeignService.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by 刘元波 on 2017/8/27.
  */
-@Service
-@FeignClient(value = "custom1",fallback = FeignService.FenginServiceImpl.class)
+@FeignClient(value = "custom1",fallback = FeignService.FeignServiceImpl.class)
 public interface FeignService {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     String hello();
 
     @Component
-    class FenginServiceImpl implements FeignService{
+    class FeignServiceImpl implements FeignService{
         @Override
         public String hello() {
             return "the custom1 is down";
